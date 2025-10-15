005771e3aa2f722508fc703869cdc8474de47844
gc <ghchen5d2012@gmail.com>
2025-10-15 14:51:17 +0800

    exercise 3.4

--- polynomials/polynomials.py
+++ polynomials/polynomials.py
@@ -50,3 +50,57 @@ class Polynomial:
 
     def __radd__(self, other):
         return self + other
+
+    def __sub__(self, other):
+        if isinstance(other, Polynomial):
+            common = min(self.degree(), other.degree()) + 1
+            coefs = tuple(a - b for a, b in zip(self.coefficients,
+                                                other.coefficients))
+            coefs += self.coefficients[common:] + other.coefficients[common:]
+
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial((self.coefficients[0] - other,)
+                              + self.coefficients[1:])
+
+        else:
+            return NotImplemented
+
+    def __rsub__(self, other):
+        return Polynomial(tuple(-i for i in (self - other).coefficients))
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            degree_sum = self.degree() + other.degree() + 1
+            coefs = [0] * degree_sum
+            for i in range(self.degree() + 1):
+                for j in range(other.degree() + 1):
+                    coefs[i + j] += self.coefficients[i] * other.coefficients[j]
+
+            return Polynomial(coefs)
+
+        elif isinstance(other, Number):
+            return Polynomial(tuple(i * other for i in self.coefficients))
+
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        return Polynomial(tuple(i * other for i in self.coefficients))
+
+    def __pow__(self, expo):
+        if isinstance(expo, Integral) and expo > 0:
+            ret = Polynomial((self.coefficients))
+            for i in range(1, expo):
+                ret *= self
+            return ret
+        else:
+            return NotImplemented
+
+    def __call__(self, x):
+        ret = 0
+        for i, c in enumerate(self.coefficients):
+            ret += c * x ** i
+        return ret
+
